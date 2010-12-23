881a4fcde111864ae6ce0072047a5ae38384097c
Barry Warsaw <barry@list.org>
2010-12-23 11:29:39 -0500

    Initial package template.

---
+++ README.txt
@@ -0,0 +1,49 @@
+Python bindings for the Mailman 3 REST API.
+
+..
+    This file is part of mailman.client.
+
+    mailman.client is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    mailman.client is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with mailman.client.  If not, see <http://www.gnu.org/licenses/>.
+
+
+==============
+mailman.client
+==============
+
+The ``mailman.client`` library provides official Python bindings for the GNU
+Mailman 3 REST API.
+
+
+Requirements
+============
+
+``mailman.client`` requires Python 2.6 or newer.
+
+
+Project details
+===============
+
+You may download the latest version of the package from the Python
+`Cheese Shop`_ or from Launchpad_.
+
+You can also install it via ``easy_install`` or ``pip``.
+
+    % sudo easy_install mailman.client
+    % sudo pip install mailman.client
+
+See the Launchpad project page for access to the Bazaar branch, bug report,
+etc.
+
+
+.. _`Cheese Shop`: http://pypi.python.org/mailman.client
+.. _Launchpad: https://launchpad.net/mailman.client
---
+++ setup.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2010 by the Free Software Foundation, Inc.
+#
+# This file is part of mailman.client.
+#
+# mailman.client is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# mailman.client is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mailman.client.  If not, see <http://www.gnu.org/licenses/>.
+
+import distribute_setup
+distribute_setup.use_setuptools()
+
+from setup_helpers import (
+    description, find_doctests, get_version, long_description, require_python)
+from setuptools import setup, find_packages
+
+
+require_python(0x20600f0)
+__version__ = get_version('mailman/client/__init__.py')
+
+
+setup(
+    name='mailman.client',
+    version=__version__,
+    namespace_packages=['mailman'],
+    packages=find_packages(),
+    include_package_data=True,
+    maintainer='Barry Warsaw',
+    maintainer_email='barry@list.org',
+    description=description('README.txt'),
+    long_description=long_description(
+        'mailman/client/README.txt',
+        'mailman/client/NEWS.txt'),
+    license='LGPLv3',
+    url='http://launchpad.net/mailman.client',
+    download_url='https://launchpad.net/mailman.client/+download',
+    test_suite='mailman.client.tests',
+    # Auto-conversion to Python 3.
+    use_2to3=True,
+    convert_2to3_doctests=find_doctests(),
+    )

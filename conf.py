ba591a4529a077aba7b49c5a929f4239ab17bb11
Jonas Meurer <jonas@freesources.org>
2017-10-26 23:26:55 +0200

    Make sphinx-build ignore .pc directory
    The quilt patch management tool might create the '.pc' directory during
    the package build process. In case that '.rst' files are patched by
    quilt, they end up in '.pc' as well. A sphinxdoc recusively scans the
    sources for '.rst' files, this causes unwanted behaviour. This patch
    adds '.pc' to the list of excluded patterns.

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ release = _version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '.tox', 'eggs']
+exclude_patterns = ['_build', '.tox', 'eggs', '.pc']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
